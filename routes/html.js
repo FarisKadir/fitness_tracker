9d57336a8cc2a197c6d835f73299477df524d519
Faris Kadir <faris.kadir@gmail.com>
2021-04-20 10:51:58 -0400

    Created html routes.

---
+++ routes/html.js
@@ -0,0 +1,15 @@
+const router = require("express").Router();
+const path = require("path");
+
+//HTML route to the exercise.html file
+router.get("/exercise", (req.res) =>  {
+    res.sendFile(path.join(_dirname, "../public/exercist.html"));
+});
+
+//HTML route to the exercise.html file
+router.get("/stats", (req.res) =>  {
+    res.sendFile(path.join(_dirname, "../public/stats.html"));
+});
+
+
+module.exports = router;
