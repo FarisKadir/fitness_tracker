625d12bdfab50aa9add1890d77159217a76e3024
Faris Kadir <faris.kadir@gmail.com>
2021-04-20 10:56:31 -0400

    Fixed html.js file, the dirname variable was not spelled correctly.

--- routes/html.js
+++ routes/html.js
@@ -3,12 +3,12 @@ const path = require("path");
 
 //HTML route to the exercise.html file
 router.get("/exercise", (req,res) =>  {
-    res.sendFile(path.join(_dirname, "../public/exercise.html"));
+    res.sendFile(path.join(__dirname, "../public/exercise.html"));
 });
 
 //HTML route to the exercise.html file
 router.get("/stats", (req,res) =>  {
-    res.sendFile(path.join(_dirname, "../public/stats.html"));
+    res.sendFile(path.join(__dirname, "../public/stats.html"));
 });
 
 
