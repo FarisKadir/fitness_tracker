55e5fc6b50f48f8b73672dfceb945bcbae68caf9
Faris Kadir <faris.kadir@gmail.com>
2021-04-20 10:52:22 -0400

    Fixed html routes.

--- routes/html.js
+++ routes/html.js
@@ -2,12 +2,12 @@ const router = require("express").Router();
 const path = require("path");
 
 //HTML route to the exercise.html file
-router.get("/exercise", (req.res) =>  {
-    res.sendFile(path.join(_dirname, "../public/exercist.html"));
+router.get("/exercise", (req,res) =>  {
+    res.sendFile(path.join(_dirname, "../public/exercise.html"));
 });
 
 //HTML route to the exercise.html file
-router.get("/stats", (req.res) =>  {
+router.get("/stats", (req,res) =>  {
     res.sendFile(path.join(_dirname, "../public/stats.html"));
 });
 
